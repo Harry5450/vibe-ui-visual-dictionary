619a42f31c91b588273c61ad03ea078409b0ea89
Harry5450 <sblack5450@gmail.com>
2026-08-04 09:26:25 +0800

    Add feedback inbox route

---
+++ app/feedback/page.tsx
@@ -0,0 +1,11 @@
+import { requireChatGPTUser } from "../chatgpt-auth";
+import FeedbackDashboard from "./FeedbackDashboard";
+
+export const dynamic = "force-dynamic";
+
+export default async function FeedbackPage() {
+  const user = await requireChatGPTUser("/feedback");
+
+  return <FeedbackDashboard initialFeedback={[]} adminName={user.displayName} />;
+}
+
